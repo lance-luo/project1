1c340e44864471f626b035b5a7556cbda09ce330
Cloud User <centos@ip-172-31-20-103.ap-southeast-2.compute.internal>
2017-05-18 03:01:23 +0000

    Project1 for python unit test and code coverage.

---
+++ project1/authentication.py
@@ -0,0 +1,19 @@
+#/usr/bin/env python
+"""This module provides functions for authenticating users."""
+
+def login(username, password):
+    try:
+        user_file = open('/etc/users.txt')
+        user_buf = user_file.read()
+        users = [line.split("|") for line in user_buf.split("\n")]
+        if [username, password] in users:
+            return True
+        else:
+            return False
+    except Exception, exc:
+        print "I can't authenticate you."
+        return False
+        
+def logout():
+    print 'this line will not be covered by test cases.'
+    print 'this line will not be covered by test cases as well.'
